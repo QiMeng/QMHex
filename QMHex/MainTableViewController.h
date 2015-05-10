22dba0e93b131a90ab8cc3a018ae92046e41ad65
QiMENG <616684889@qq.com>
2015-05-10 11:33:51 +0800

    main

---
+++ QMHex/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  QMHex
+//
+//  Created by Lin on 15/5/9.
+//  Copyright (c) 2015年 QiMENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
---
+++ QMHex/MainTableViewController.m
@@ -0,0 +1,19 @@
+//
+//  MainTableViewController.m
+//  QMHex
+//
+//  Created by Lin on 15/5/9.
+//  Copyright (c) 2015年 QiMENG. All rights reserved.
+//
+
+#import "MainTableViewController.h"
+
+@implementation MainTableViewController
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 40;
+}
+
+
+
+@end
