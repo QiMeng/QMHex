954ff1871d54e1d232ff974a254229d60701d8bd
QiMENG <616684889@qq.com>
2015-05-16 09:04:46 +0800

    cocoalumberjack

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, "7.0"
+
+pod 'CocoaLumberjack', '~> 2.0.0'
+
+target :QMHexTests, :exclusive => true do
+  # pod 'Kiwi'
+end
--- QMHex/AppDelegate.m
+++ QMHex/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import <CocoaLumberjack.h>
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,15 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+//    setenv("XcodeColors", "YES", 0);
+//    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+//    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];// 启用颜色区分
+//    DDLogError(@"错误信息"); // 红色
+//    DDLogWarn(@"警告"); // 橙色
+//    DDLogInfo(@"提示信息"); // 默认是黑色
+//    DDLogVerbose(@"详细信息"); // 默认是黑色
+    
     return YES;
 }
 
