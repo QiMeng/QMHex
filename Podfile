270495f007cdd14ab7bd97f57a645ad5062411c8
QiMENG <616684889@qq.com>
2015-05-18 11:01:30 +0800

    add among

--- Podfile
+++ Podfile
@@ -2,6 +2,7 @@ platform :ios, "7.0"
 
 pod 'CocoaLumberjack', '~> 2.0.0'
 pod 'SVProgressHUD', '~> 1.1.3'
+pod 'UMengAnalytics-NO-IDFA', '~> 3.4.7'
 
 
 target :QMHexTests, :exclusive => true do
--- QMHex/AppDelegate.m
+++ QMHex/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <MobClick.h>
 @interface AppDelegate ()
 
 @end
@@ -16,10 +17,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //5559512a67e58efd54005aac
+    [MobClick startWithAppkey:@"" reportPolicy:BATCH   channelId:@"ceshi"];
     
-    setenv("XcodeColors", "YES", 0);
-    [DDLog addLogger:[DDTTYLogger sharedInstance]];
-    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];// 启用颜色区分
+    
+//    setenv("XcodeColors", "YES", 0);
+//    [DDLog addLogger:[DDTTYLogger sharedInstance]];
+//    [[DDTTYLogger sharedInstance] setColorsEnabled:YES];// 启用颜色区分
 //    DDLogError(@"错误信息"); // 红色
 //    DDLogWarn(@"警告"); // 橙色
 //    DDLogInfo(@"提示信息"); // 默认是黑色
