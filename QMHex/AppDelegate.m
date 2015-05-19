dd496c74720553775ab17b4159c183b681dabb79
QiMENG <616684889@qq.com>
2015-05-19 14:02:43 +0800

    icon

--- QMHex/AppDelegate.m
+++ QMHex/AppDelegate.m
@@ -19,7 +19,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    [MobClick startWithAppkey:kUMengKey reportPolicy:BATCH   channelId:@"ceshi"];
+    [MobClick startWithAppkey:kUMengKey reportPolicy:BATCH   channelId:@"AppStore"];
     [UMSocialData setAppKey:kUMengKey];
     
     
